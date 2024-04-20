a5daa63fcec7f5b051ba5bfd939ccdd2fa830557
Artem <plishkan25@gmail.com>
2024-04-20 17:17:50 +0300

    fix basename route

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/InventoryTest/">
     <Provider store={store}>
       <App />
     </Provider>
